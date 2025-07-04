ebdc8151c6e34e699e036bf9a31b0c2af2894614
dlux-tech-ravi <ravichandran.s@dluxtech.com>
2025-07-04 15:58:38 +0530

    next config file updated

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.stories\.(js|jsx|ts|tsx|mdx)$/,
+      use: 'null-loader',
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
